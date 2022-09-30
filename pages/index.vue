86e10afaa38abff36167b7a76a64508582b4894d
natthanun1999 <natthanun1999@gmail.com>
2022-09-30 18:49:06 +0700

    :tada: First init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <Tutorial/> -->
+  <div class="container h-screen mx-auto flex justify-center items-center">
+    <span class="text-5xl">
+      Gentlewoman
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
